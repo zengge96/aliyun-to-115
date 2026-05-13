d85ae7ed6be9b6c00f839c5a9baa98d7e8624542
zengge96 <zengge99@gmail.com>
2026-05-13 15:12:04 +0800

    ok

--- drivers/aliyun_to_115/sync.go
+++ drivers/aliyun_to_115/sync.go
@@ -591,7 +591,8 @@ func (d *AliyunTo115) processSingleFile(ctx context.Context, srcPath string, dst
 	// 规避115 Share List的Size错误
 	fileSize :=  f.GetSize()
 	fmt.Printf("fileSize1:%d\n", fileSize)
-	fmt.Printf("[DEBUG] aliyun interface type: %T，privider：%s\n", aliyun, f.GetProvider())
+	provider, ok := model.GetProvider(f)
+	fmt.Printf("[DEBUG] aliyun interface type: %T，privider：%s\n", aliyun, provider)
 	if _, ok := aliyun.(*_115_share.Pan115Share); ok {
 		req, _ := http.NewRequestWithContext(ctx, http.MethodHead, link.URL, nil)
 		resp, err := http.DefaultClient.Do(req)

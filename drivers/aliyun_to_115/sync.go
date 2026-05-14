a2f3f5b3f22e5a3d515b374c32bb2b7cccf0b8e1
zengge96 <zengge99@gmail.com>
2026-05-14 17:19:02 +0800

    ok

--- drivers/aliyun_to_115/sync.go
+++ drivers/aliyun_to_115/sync.go
@@ -328,7 +328,7 @@ func (d *AliyunTo115) doSync() {
 
 	// 使用 fs.List 遍历所有文件，按 provider 白名单过滤
 	fmt.Println("[aliyun_to_115] 开始通过fs.List遍历文件...")
-	d.fsWalkAndSync(ctx, "/每日更新/动漫/国漫", stats, breakpointPath, &fullScan, db2)
+	d.fsWalkAndSync(ctx, "/每日更新/动漫/国漫/所有", stats, breakpointPath, &fullScan, db2)
 
 	clearBreakpoint(db2)
 
@@ -468,9 +468,9 @@ func (d *AliyunTo115) fsWalkAndSync(ctx context.Context, currentPath string, sta
 
 		if f.IsDir() {
 			subPath := currentPath + f.GetName() + "/"
-			if !strings.HasPrefix(subPath, "/每日更新/动漫/国漫/所有") {
-				continue
-			}
+			// if !strings.HasPrefix(subPath, "/每日更新/动漫/国漫/所有") {
+			// 	continue
+			// }
 			if !(*fullScan) {
 				if !strings.HasPrefix(breakpointPath, subPath) {
 					continue
